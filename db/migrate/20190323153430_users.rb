d0acc64fa0d46241ef9be812a47cbfe32d11567f
Nathalie Santana <nathalie.scruz@gmail.com>
2019-03-23 15:54:19 +0000

    Migrate Tables.

--- db/migrate/20190323153430_users.rb
+++ db/migrate/20190323153430_users.rb
@@ -1,4 +1,9 @@
 class Users < ActiveRecord::Migration
   def change
+    create_table :create_users do |t|
+      t.string :username
+      t.text :email
+      t.string :password_digest
+    end
   end
 end
--- db/migrate/20190323153515_custom_beverages.rb
+++ db/migrate/20190323153515_custom_beverages.rb
@@ -1,4 +1,8 @@
 class CustomBeverages < ActiveRecord::Migration
   def change
+    create_table :create_custom_beverages do |t|
+      t.string :name
+      t.text :ingredients
+    end
   end
 end
